5f602a3c4891adb2173d9242f385d3dab1b36ec4
Oliver <olivercreativeworks@gmail.com>
2024-06-18 07:49:36 -0400

    Move displayGameboard function into display module

--- playGame/display.js
+++ playGame/display.js
@@ -93,6 +93,53 @@ function setupDisplay(game){
 
     gameHistory.update(gameboard)
   }
+
+  /**
+   * @param {SpreadsheetApp.Sheet} _display
+   * @param {Gameboard} gameboard
+   */
+  function displayOthelloGameboard(_display, gameboard = createGameboard()){
+    const display = make8x8(_display)
+      
+    const displayRange = display.getRange(1, 1, 8, 8)
+      .clear()
+      .setVerticalAlignments(Array.from({length:8}, _ => Array(8).fill("middle")))
+      .setHorizontalAlignments(Array.from({length:8}, _ => Array(8).fill("center")))
+      .setValues(gameboard)
+      .setBackground("#fce5cd")
+    
+    const emptySpaceRule = SpreadsheetApp.newConditionalFormatRule()
+      .whenCellEmpty().setBackground("#fce5cd")
+      .setRanges([displayRange])
+      .build()
+
+    const blackRule = SpreadsheetApp.newConditionalFormatRule()
+      .whenTextEqualTo(BLACK).setBackground("#434343").setFontColor("black")
+      .setRanges([displayRange])
+      .build()
+    const whiteRule = SpreadsheetApp.newConditionalFormatRule()
+      .whenTextEqualTo(WHITE).setBackground("#f3f3f3").setFontColor("white")
+      .setRanges([displayRange])
+      .build()
+    display.setConditionalFormatRules(display.getConditionalFormatRules().concat(emptySpaceRule, blackRule, whiteRule))
+    return display
+
+    /**
+     * @param {SpreadsheetApp.Sheet} display
+     */
+    function make8x8(display){
+      if(display.getMaxColumns() >= 8 && display.getMaxRows() >= 8) return display
+      display.deleteColumns(1, display.getMaxColumns() - 1)
+      display.deleteRows(1, display.getMaxRows() - 1)
+
+      display.insertColumns(1, 7)
+      display.insertRows(1, 7)
+      
+      display.setRowHeights(1, 8, 50)
+      display.setColumnWidths(1, 8, 50)
+      return display
+    }
+  }
 }
 
 function getBoardHistory(){
